a542764d93cdea0206fd662953550d6eae7063ce
Nouhaila <charafnouhaila10@gmail.com>
2026-06-28 23:11:39 +0100

    Modif5

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -15,7 +15,17 @@ setListData(dataObject)
 },[])
 
 let total=0;
+ listData.forEach((item)=>{
+    total += item.price
+ })
 
+ const DeleteProduct=(idToDelete)=>{
+const dataUpdate =listData.filter((item)=>item.id !== idToDelete)
+setListData(dataUpdate)
+
+localStorage.setItem("product",JSON.stringify(dataUpdate))
+ }
+ 
 return(
 
     <div className="max-w-6xl mx-auto p-6">
@@ -46,12 +56,30 @@ return(
             </div>
 
             
-            <button className="px-5 py-2 bg-red-600 text-white rounded-lg hover:bg-blue-700 transition">
+            <button className="px-5 py-2 bg-red-600 text-white rounded-lg hover:bg-blue-700 transition" value={product.id} onClick={()=>DeleteProduct(product.id)}>
               Delete
             </button>
           </div>
         ))}
       </div>
+      <div className="mt-8 bg-white shadow rounded-xl p-6">
+
+        <div className="flex justify-between items-center border-b pb-4">
+          <span className="text-xl font-semibold">
+            Total
+          </span>
+
+          <span className="text-2xl font-bold text-blue-600">
+            ${total}
+          </span>
+        </div>
+
+        <button className="w-full mt-6 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition">
+          Place Order
+        </button>
+
+      </div>
+
     </div>
 )
 
